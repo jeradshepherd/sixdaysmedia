4297fb4128011a8aef2fc2fe8a0d79221a05608b
jeradshepherdrv <jshepherd@redventures.com>
2017-04-11 19:25:07 -0400

    Update templates and styles. WIP

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+$('.hero--home').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true
+});
---
+++ assets/js/vendor.js
@@ -0,0 +1,2 @@
+//=include ../../global/jquery/jquery-2.1.3.min.js
+//=include vendor/slick.min.js
